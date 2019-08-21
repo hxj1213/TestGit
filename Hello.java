70a06b966ab13e92d5cf46fdb50683f76c291b2f
hxj <hxj1213@126.com>
2019-08-21 08:33:50 +0800

    update Hello.java and Student.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
      public static void main(String[] args){
              System.out.println("Hello World");
              System.out.println("第二次提交");
+             System.out.println("查看修改");
      }
 }
